d22abbe4d5c5c901ab14af6ac2e9382277cdebee
Mike Schutte <tmikeschutte@gmail.com>
2017-08-29 12:55:14 -0600

    Add test rake and clean up test

--- test/alphabet_test.rb
+++ test/alphabet_test.rb
@@ -2,16 +2,15 @@ require "./test/test_helper"
 require "./lib/alphabet"
 
 class AlphabetTest < Minitest::Test
+  def alphabet
+    @alphabet ||= Alphabet.new
+  end
 
 	def test_number_of_characters
-		alphabet = Alphabet.new
-
 		assert_equal 35, alphabet.braille_letter_hash.count
 	end
 
 	def test_hash_values_as_braille
-		alphabet = Alphabet.new
-
 		alphabet.braille_letter_hash.each do | k, v|
 			assert_equal 6, v.length
 			v.chars.each do |c|
